b9b797c912161dea20bd9d85c7124da3867c5f46
Joseph Rushton Wakeling <joe@webdrake.net>
2010-05-11 19:51:36 +0200

    Updates to licensing/copyright notices and README/INSTALL/NEWS.

--- sampling/gsl_sampling.h
+++ sampling/gsl_sampling.h
@@ -1,33 +1,24 @@
 /* sampling/gsl_sampling.h
  *
+ * ---------------------------------------------------------------------
+ * Header file for GrSL, designed to facilitate long-term incorporation
+ * into the GNU Scientific Library.
+ * ---------------------------------------------------------------------
+ *
  * Copyright (C) 2010 Joseph Rushton Wakeling
  *
- * This program is free software; you can redistribute it and/or modify
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or (at
- * your option) any later version.
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- *
- *
- * --REFERENCES--
- *
- *   Nair KA (1990) 'An improved algorithm for ordered sequential
- *     random sampling.'  ACM T. Math. Softw. 16(3): 269--274.
- *
- *   Vitter JS (1984) 'Faster methods for random sampling.'
- *     Commun. ACM 27(7): 703--718
- *
- *   Vitter JS (1987) 'An efficient algorithm for sequential random
- *     sampling.'  ACM T. Math. Softw. 13(1): 58--67.
- *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef __GSL_SAMPLING_H__
